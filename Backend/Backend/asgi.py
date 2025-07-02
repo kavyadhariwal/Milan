13ffdd6152616c2df4861f78e3959a50d2ca7d24
kavyadhariwal <kavyadhariwal164@gmail.com>
2025-07-02 14:17:11 +0530

    deploy commit

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+settings_module='Backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'Backend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
--- Backend/Backend/wsgi.py
+++ Backend/Backend/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+settings_module='Backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'Backend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
